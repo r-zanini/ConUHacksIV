61fd600464f37fed81a2a70ed56211e621013fde
meskandari <m.eskandari.ca@gmail.com>
2019-01-26 20:29:12 -0500

    2ndVersion

--- app/models/Services.java
+++ app/models/Services.java
@@ -6,7 +6,10 @@ public class Services {
     private String serviceName;
     private String serviceID;
     private boolean englishConfirmed=false;
+    private boolean englishNotConfirmed=false;
     private boolean frenchConfirmed=false;
+    private boolean frenchNotConfirmed=false;
+
     private String serviceCategory;
     private float serviceScore;
     
@@ -29,8 +32,14 @@ public class Services {
     public boolean getEngConfirmed(){
         return englishConfirmed;
     }
+    public boolean getEngNConfirmed(){
+        return englishNotConfirmed;
+    }
     public boolean getFrConfirmed(){
         return frenchConfirmed;
+    }
+    public boolean getFrNConfirmed(){
+        return frenchNotConfirmed;
     }
     public String getServiceCategory(){
         return serviceCategory;
@@ -47,8 +56,14 @@ public class Services {
     public void setEngConfirm(boolean engConfirm){
         englishConfirmed=engConfirm;
     }
+    public void setEngNConfirm(boolean engNConfirm){
+        englishNotConfirmed=engNConfirm;
+    }
     public void setFrConfirm(boolean frConfirm){
         frenchConfirmed=frConfirm;
+    }
+    public void setFrNConfirm(boolean frNConfirm){
+        frenchNotConfirmed=frNConfirm;
     }
     public void setServiceScore(float srvScore){
         serviceScore=srvScore;
